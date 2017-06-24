cde03883318b43c1460eca9bb19da55002258bfc
Carlos Gómez <carliros.g@gmail.com>
2017-06-23 21:26:06 -0400

    Initial version

---
+++ README.md
@@ -0,0 +1,3 @@
+# Generate Pandoc files from Coq (v2md)
+
+The idea is just to generate pandoc files from Coq (.v) files.
---
+++ src/Main.hs
@@ -0,0 +1,37 @@
+module Main where
+
+import           Data.List
+import           Data.Semigroup      ((<>))
+import           Numeric
+import           Options.Applicative
+import           System.Environment
+import           System.FilePath
+import           System.IO
+
+data V2MdOptions
+  = V2MdOptions { output :: Maybe String
+                , source :: String
+                }
+
+pV2Md :: Parser V2MdOptions
+pV2Md = V2MdOptions
+  <$> optional (strOption (long "output" <> short 'o' <> metavar "FILE" <> help "Write output to FILE"))
+  <*> strArgument (metavar "SOURCE")
+
+processOptions :: V2MdOptions -> IO ()
+processOptions (V2MdOptions Nothing src)
+  = processVFile (replaceExtension src "md") src
+processOptions (V2MdOptions (Just output) src)
+  = processVFile output src
+
+processVFile :: String -> String -> IO()
+processVFile output src
+  = do putStrLn src
+       putStrLn output
+
+main :: IO ()
+main = execParser opts >>= processOptions
+  where
+    opts = info (helper <*> pV2Md)
+                (fullDesc <> progDesc "Generate Pandoc files from Coq (.v)"
+                          <> header "v2md (Coq to Pandoc)")
